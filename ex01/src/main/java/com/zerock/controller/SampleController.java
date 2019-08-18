653464d54ded4b0253957185a97e12a9e7c7c226
happy16123 <gustn16113@naver.com>
2019-08-18 18:00:47 +0900

    임시

--- ex01/src/main/java/com/zerock/controller/SampleController.java
+++ ex01/src/main/java/com/zerock/controller/SampleController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zerock.domain.SampleDTO;
 import com.zerock.domain.TodoDTO;
@@ -81,5 +82,16 @@ public class SampleController {
 			log.info("dto : " + dto);
 			log.info("page : " + page);
 			return "/sample/ex04";
+		}
+		
+		@GetMapping("ex06") 
+		public @ResponseBody SampleDTO ex06() {
+			log.info("ex06...............");
+			
+			SampleDTO dto = new SampleDTO();
+			dto.setAge(10);
+			dto.setName("aaa");
+			
+			return dto;
 		}
 }
