bb70b9ef5c98263faac78ceb716d7ead2d9d0713
happy16123 <51432536+happy16123@users.noreply.github.com>
2019-08-20 23:19:35 +0900

    게시판 만들기 기본세팅

---
+++ board/src/main/java/com/zerock/config/RootConfig.java
@@ -0,0 +1,38 @@
+package com.zerock.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@MapperScan(basePackages = {"com.zerock.mapper"})
+public class RootConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikari = new HikariConfig();
+		hikari.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikari.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:xe");
+		hikari.setUsername("scott");
+		hikari.setPassword("tiger");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikari);
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean session = new SqlSessionFactoryBean();
+		session.setDataSource(dataSource());
+		
+		return (SqlSessionFactory)session.getObject();
+	}
+}
