f56ac730f924dd064e9032ac8da62aefd89c18e4
happy16123 <51432536+happy16123@users.noreply.github.com>
2019-08-21 22:26:05 +0900

    crud test

--- board/src/test/java/com/zerock/mapper/BoardMapperTests.java
+++ board/src/test/java/com/zerock/mapper/BoardMapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.zerock.domain.BoardVO;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -20,5 +22,49 @@ public class BoardMapperTests {
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("test title");
+		board.setContent("test content");
+		board.setWriter("test writer");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("test title select key");
+		board.setContent("test content select key");
+		board.setWriter("test writer select key");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(5L);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		
+		board.setBno(5L);
+		board.setTitle("modify title");
+		board.setContent("modfiy content");
+		board.setWriter("modify writer");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT : " + count);
 	}
 }
