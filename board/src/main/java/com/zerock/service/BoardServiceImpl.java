d26521fd62392c934a135c9cda90c009662525c8
happy16123 <51432536+happy16123@users.noreply.github.com>
2019-09-18 22:22:24 +0900

    페이저처리완료

--- board/src/main/java/com/zerock/service/BoardServiceImpl.java
+++ board/src/main/java/com/zerock/service/BoardServiceImpl.java
@@ -50,6 +50,12 @@ public class BoardServiceImpl implements BoardService {
 		log.info("ger List with criteria : " + cri);
 		return mapper.getListWithPaging(cri);
 	}
+	
+	@Override
+	public int getTotal(Criteria cri) {
+		log.info("get total count");
+		return mapper.getTotalCount(cri);
+	}
 
 //	@Override
 //	public List<BoardVO> getList() {
