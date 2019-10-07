b5e9b4ca09e9f28a8590cd1dd82ea09d225b088b
happy16123 <51432536+happy16123@users.noreply.github.com>
2019-10-07 22:05:02 +0900

    파일업로드

---
+++ fileupload/src/main/java/com/zerock/config/WebConfig.java
@@ -0,0 +1,48 @@
+package com.zerock.config;
+
+import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		// TODO Auto-generated method stub
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+		
+		MultipartConfigElement multipartConfig = new MultipartConfigElement("C:\\Users\\gustn\\Desktop\\spring-start\\upload\\temp", 20971520, 41943040, 20971520);
+		registration.setMultipartConfig(multipartConfig);
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		// TODO Auto-generated method stub
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		return new Filter[] { filter };
+	}
+
+}
