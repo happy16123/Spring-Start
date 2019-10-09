67c3463f17d733b56b8764654f98045080b24c3f
happy16123 <51432536+happy16123@users.noreply.github.com>
2019-10-09 20:55:45 +0900

    썸네일

--- fileupload/src/main/java/com/zerock/controller/UploadController.java
+++ fileupload/src/main/java/com/zerock/controller/UploadController.java
@@ -1,14 +1,25 @@
 package com.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -42,22 +53,57 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxAction(MultipartFile[] uploadFile) {
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<String> uploadAjaxAction(MultipartFile[] uploadFile) {
 		log.info("update ajax post");
 		String uploadFolder = "C:\\Users\\gustn\\Desktop\\spring-start\\upload";
+		File uploadPath = new File(uploadFolder, getFolder());
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		
 		for(MultipartFile multipartFile : uploadFile) {
 			log.info("------------------------------");
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
 			
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			UUID uuid = UUID.randomUUID();
+			String uploadFileName = uuid.toString() + "_" + multipartFile.getOriginalFilename();
+			
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
+				if(checkImageType(saveFile)) {
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				}
 			} catch(Exception e) {
 				log.error(e.getMessage());
 			}
+			
 		}
+		return new ResponseEntity<String>("success", HttpStatus.OK);
+	}
+	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		
+		return str.replace("-", File.separator);
+	}
+	
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			return contentType.startsWith("image");
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return false; 
 	}
 }
