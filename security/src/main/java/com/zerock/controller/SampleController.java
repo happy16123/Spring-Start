a1d60632ab8a87cb6d857f04e42b6e6a5e6c2b6f
happy16123 <51432536+happy16123@users.noreply.github.com>
2019-10-27 22:15:06 +0900

    security권한

--- security/src/main/java/com/zerock/controller/SampleController.java
+++ security/src/main/java/com/zerock/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.zerock.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,5 +26,11 @@ public class SampleController {
 	@GetMapping("/admin")
 	public void doAdmin() {
 		log.info("admin only");
+	}
+	
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+		log.info("access Denied : " + auth);
+		model.addAttribute("msg", "Access Denied");
 	}
 }
