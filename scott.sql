30d9962f75ec745f354f579f9874e2b4bdfde431
happy16123 <51432536+happy16123@users.noreply.github.com>
2019-10-31 14:11:41 +0900

    jdbc 로그인

--- scott.sql
+++ scott.sql
@@ -71,3 +71,18 @@ insert into authorities (username, authority) values('user00', 'ROLE_USER');
 insert into authorities (username, authority) values('member00', 'ROLE_MANAGER');
 insert into authorities (username, authority) values('admin00', 'ROLE_MANAGER');
 insert into authorities (username, authority) values('admin00', 'ROLE_ADMIN');
+
+create table tbl_member(
+    userid varchar2(50) not null primary key,
+    userpw varchar2(50) not null,
+    username varchar2(100) not null,
+    regdate date default sysdate,
+    updatedate date default sysdate,
+    enabled char(1) default '1'
+);
+
+create table tbl_member_auth(
+    userid varchar2(50) not null,
+    auth varchar2(50) not null,
+    constraint fk_member_auth foreign key(userid) references tbl_member(userid)
+);
